85c5a2c39a15973ab358c57ba01521364ddbbfe4
cargo-cmyk <carolin.goetz@arbor-insight.com>
2025-12-09 11:05:33 +0100

    add ai api

--- src/script.js
+++ src/script.js
@@ -1,12 +1,27 @@
-function generatePoem(event) {
-  event.preventDefault();
-
+function displayPoem(response) {
   new Typewriter("#poem", {
-    strings: "La tombe dit à la rose",
+    strings: response.data.answer,
     autoStart: true,
     delay: 1,
     cursor: "",
   });
+}
+
+function generatePoem(event) {
+  event.preventDefault();
+
+  let instructionsInput = document.querySelector("#user-instructions");
+  let apiKey = "a3fbf9429t7a57989ac706ao0d0b154f";
+  let context =
+    "You are a witty Poem expert and love to write funny poems. Your mission is to generate a 4 line poem in basic HTML and separate each line with a <br />. Make sure to follow the user instructions. Do not include a title to the poem. Sign the poem with 'I love Apples' inside a <strong> element at the end of the poem and NOT at the beginning. Make sure to always include Apples in the poem but in a very smart way.";
+  let prompt = `User instructions: Generate a German poem about ${instructionsInput.value}`;
+  let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  let poemElement = document.querySelector("#poem");
+  poemElement.classList.remove("hidden");
+  poemElement.innerHTML = `<div class="generating">⏳ Generating a German poem about ${instructionsInput.value}</div>`;
+
+  axios.get(apiURL).then(displayPoem);
 }
 
 let poemFormElement = document.querySelector("#poem-generator-form");
